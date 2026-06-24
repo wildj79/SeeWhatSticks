fc20887dc8b366d0f719c1d75b76835e839a1313
James Allred <wildj79@gmail.com>
2026-06-24 07:57:11 -0500

    Adding in some of the item logic and creating a basic scene for testing

--- src/core/main_game/main_game.gd
+++ src/core/main_game/main_game.gd
@@ -3,6 +3,8 @@ extends Node
 
 @export var item_definitions: Array[ItemDefinition]
 @export var modifier_definintions: Array[ModifierDefinition]
+@export var wall_scene: PackedScene
+@export var item_scene: PackedScene
 
 # World Roots
 @onready var level_root: Node2D = %LevelRoot
@@ -14,3 +16,13 @@ extends Node
 @onready var pause_root: Control = %PauseRoot
 @onready var transition_root: Control = %TransitionRoot
 @onready var debug_root: Control = %DebugRoot
+
+func _ready() -> void:
+	var wall = wall_scene.instantiate()
+	var item = item_scene.instantiate()
+	var rect = get_viewport().get_visible_rect()
+	wall.position = rect.get_center() + Vector2(50, 0)
+	item.position = rect.get_center() - Vector2(50, 0)
+	item.set_item_definition(item_definitions[randi() % item_definitions.size()])
+	entity_root.add_child(wall)
+	entity_root.add_child(item)
--- src/gameplay/item/item.gd
+++ src/gameplay/item/item.gd
@@ -1,6 +1,19 @@
-extends CharacterBody2D
+extends Area2D
 
 @export var item_definition: ItemDefinition
 
 func set_item_definition(item: ItemDefinition) -> void:
-	self.item_definition = item
+	item_definition = item
+	print(item_definition)
+
+func _process(delta: float) -> void:
+	position += 100 * delta * Vector2.RIGHT
+
+func _on_body_entered(body: Node2D) -> void:
+	if not item_definition:
+		printerr("Shit be broken")
+		return
+		
+	var chance: float = clampf(item_definition.adhesion_factor - item_definition.weight, 5, 95)
+	print("A %s has a %.1f%% chance to stick" % [item_definition.name, chance])
+	print(randf() * 100 <= chance)
